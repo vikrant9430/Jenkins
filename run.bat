8f4771f74652aafc0c418a3ffefe4598c121f66a
Vikrant Nandan <vikrant1089@gmail.com>
2018-01-30 10:38:26 +0530

    First Commit

---
+++ run.bat
@@ -0,0 +1,3 @@
+cd E:\Batch 1130\jenkins
+
+java -cp E:\Batch 1130\jenkins\lib*;E:\Batch 1130\jenkins\bin org.testng.TestNG testng.xml
---
+++ src/jenkins/JenkinsExample.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class JenkinsExample {
+	
+	@Test
+	public void JenkinTest(){
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
